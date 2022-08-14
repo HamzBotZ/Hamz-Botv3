6c4054ce76b47ea110882ec5be7cf9ddb11f82c9
HamzGanz <105532183+HamzBotZ@users.noreply.github.com>
2022-08-14 13:08:37 +0700

    Update zimbot.js
    Signed-off-by: HamzGanz <105532183+HamzBotZ@users.noreply.github.com>

--- zimbot.js
+++ zimbot.js
@@ -6709,7 +6709,7 @@ case 'listmenu': case 'list': {
 ┃https://tinyurl.com/2bghgjfx
 ┃
 ┃━━━━━━━━━━━━━━━━━━┃
-┃                  ᴢɪᴍʙᴏᴛɪɴᴄ
+┃                  ZxynnBotz
 ╰━━━━━━━━━━━━━━━━━━╯`,
        footer: ZimBotInc.user.name,
        title: `${ucapannya2} ${pushname}`,
@@ -6752,7 +6752,7 @@ anu = `*✧ᴜᴘᴛɪᴍᴇ: ${runtime(process.uptime())}*
 ┃https://tinyurl.com/2bghgjfx
 ┃
 ┃━━━━━━━━━━━━━━━━━━┃
-┃                  ᴢɪᴍʙᴏᴛɪɴᴄ
+┃                  ZxynnBotz
 ╰━━━━━━━━━━━━━━━━━━╯
 
     
@@ -7353,7 +7353,7 @@ url: 'https://www.youtube.com/c/Hamz9999+'
 }, {
 urlButton: {
 displayText: 'GITHUB',
-url: 'https://github.com/zim-bot/zimbot-v3'
+url: 'https://github.com/HamzBotzzzzz'
 }
 }, {
 quickReplyButton: {
@@ -7386,12 +7386,12 @@ hydratedFooterText: `${botname}`,
 hydratedButtons: [{
 urlButton: {
 displayText: 'SUBSCRIBE',
-url: 'https://www.youtube.com/c/DRIPSOFC'
+url: 'https://www.youtube.com/c/Hamz9999+'
 }
 }, {
 urlButton: {
 displayText: 'GITHUB',
-url: 'https://github.com/zim-bot/zimbot-v3'
+url: 'https://github.com/HamzBotzzzzz'
 }
 }, {
 quickReplyButton: {
@@ -7424,12 +7424,12 @@ ZimBotInc.relayMessage(m.chat, template.message, { messageId: template.key.id })
 hydratedButtons: [{
 urlButton: {
 displayText: 'SUBSCRIBE',
-url: 'https://www.youtube.com/c/DRIPSOFC'
+url: 'https://www.youtube.com/c/Hamz9999+'
 }
 }, {
 urlButton: {
 displayText: 'GITHUB',
-url: 'https://github.com/zim-bot/zimbot-v3'
+url: 'https://github.com/HamzBotzzzzz'
 }
 }, {
 quickReplyButton: {
@@ -7494,7 +7494,7 @@ ZimBotInc.relayMessage(m.chat, template.message, { messageId: template.key.id })
 ┃https://tinyurl.com/2bghgjfx
 ┃
 ┃━━━━━━━━━━━━━━━━━━┃
-┃                  ᴢɪᴍʙᴏᴛɪɴᴄ
+┃                  ZxynnBotz
 ╰━━━━━━━━━━━━━━━━━━╯`,
        footer: `ZxynnBotz`,
        title: `*LIST MENU*`,
@@ -7601,7 +7601,7 @@ let hao = randomNomor(200)
 ┃https://tinyurl.com/2bghgjfx
 ┃
 ┃━━━━━━━━━━━━━━━━━━┃
-┃                  ᴢɪᴍʙᴏᴛɪɴᴄ
+┃                  ZxynnBotz
 ╰━━━━━━━━━━━━━━━━━━╯
 
      
@@ -7649,7 +7649,7 @@ let hao = randomNomor(200)
 ┃https://tinyurl.com/2bghgjfx
 ┃
 ┃━━━━━━━━━━━━━━━━━━┃
-┃                  ᴢɪᴍʙᴏᴛɪɴᴄ
+┃                  ZxynnBotz
 ╰━━━━━━━━━━━━━━━━━━╯
 
      
@@ -7706,7 +7706,7 @@ function _0x3217(_0x37715e,_0x2f6592){const _0xa94637=_0xa946();return _0x3217=f
                 ┃https://tinyurl.com/2bghgjfx
                 ┃
                 ┃━━━━━━━━━━━━━━━━━━┃
-                ┃                  ᴢɪᴍʙᴏᴛɪɴᴄ
+                ┃                  ZxynnBotz
                 ╰━━━━━━━━━━━━━━━━━━╯
                 
                      
@@ -7751,7 +7751,7 @@ let hao = randomNomor(200)
 ┃https://tinyurl.com/2bghgjfx
 ┃
 ┃━━━━━━━━━━━━━━━━━━┃
-┃                  ᴢɪᴍʙᴏᴛɪɴᴄ
+┃                  ZxynnBotz
 ╰━━━━━━━━━━━━━━━━━━╯
 
      
@@ -7824,7 +7824,7 @@ let hao = randomNomor(200)
 ┃https://tinyurl.com/2bghgjfx
 ┃
 ┃━━━━━━━━━━━━━━━━━━┃
-┃                  ᴢɪᴍʙᴏᴛɪɴᴄ
+┃                  ZxynnBotz
 ╰━━━━━━━━━━━━━━━━━━╯
 
      
@@ -7903,7 +7903,7 @@ let hao = randomNomor(200)
 ┃https://tinyurl.com/2bghgjfx
 ┃
 ┃━━━━━━━━━━━━━━━━━━┃
-┃                  ᴢɪᴍʙᴏᴛɪɴᴄ
+┃                  ZxynnBotz
 ╰━━━━━━━━━━━━━━━━━━╯
 
      
@@ -7972,7 +7972,7 @@ let hao = randomNomor(200)
 ┃https://tinyurl.com/2bghgjfx
 ┃
 ┃━━━━━━━━━━━━━━━━━━┃
-┃                  ᴢɪᴍʙᴏᴛɪɴᴄ
+┃                  ZxynnBotz
 ╰━━━━━━━━━━━━━━━━━━╯
 
      
@@ -8049,7 +8049,7 @@ let hao = randomNomor(200)
 ┃https://tinyurl.com/2bghgjfx
 ┃
 ┃━━━━━━━━━━━━━━━━━━┃
-┃                  ᴢɪᴍʙᴏᴛɪɴᴄ
+┃                  ZxynnBotz
 ╰━━━━━━━━━━━━━━━━━━╯
 
      
@@ -8125,7 +8125,7 @@ let hao = randomNomor(200)
 ┃⬤${prefix}summerysand
 ┗━━━━━━━━━⦿
 
-©️𝗭𝗜𝗠 𝗕𝗢𝗧 𝗜𝗡𝗖
+© ZxynnBotz
 `
 function _0x186b(){const _0x16c1b5=['135080AGrzDU','Message','GITHUB','10mzHMwO','chat','21bZsKRC','552424zUneQH','1429756BqfDSd','key','240mbdKBv','relayMessage','844052JLGZth','146104dkkfWS','1857480tcaVPE','595awiboR','16150698mclKJV','botname','waUploadToServer','SUBSCRIBE'];_0x186b=function(){return _0x16c1b5;};return _0x186b();}const _0x36f452=_0x131e;(function(_0x25c54b,_0x526416){const _0x54eaa5=_0x131e,_0x24b40d=_0x25c54b();while(!![]){try{const _0x437851=-parseInt(_0x54eaa5(0x193))/0x1+-parseInt(_0x54eaa5(0x1a0))/0x2+parseInt(_0x54eaa5(0x19e))/0x3*(-parseInt(_0x54eaa5(0x19f))/0x4)+parseInt(_0x54eaa5(0x199))/0x5*(parseInt(_0x54eaa5(0x1a2))/0x6)+-parseInt(_0x54eaa5(0x194))/0x7*(-parseInt(_0x54eaa5(0x1a5))/0x8)+parseInt(_0x54eaa5(0x195))/0x9*(parseInt(_0x54eaa5(0x19c))/0xa)+parseInt(_0x54eaa5(0x1a4))/0xb;if(_0x437851===_0x526416)break;else _0x24b40d['push'](_0x24b40d['shift']());}catch(_0x459222){_0x24b40d['push'](_0x24b40d['shift']());}}}(_0x186b,0xeba1d));let message=await prepareWAMessageMedia({'image':buffer,'jpegThumbnail':buffer},{'upload':ZimBotInc[_0x36f452(0x197)]});const template=generateWAMessageFromContent(m['chat'],proto[_0x36f452(0x19a)]['fromObject']({'templateMessage':{'hydratedTemplate':{'imageMessage':message['imageMessage'],'hydratedContentText':anu,'hydratedFooterText':''+global[_0x36f452(0x196)],'hydratedButtons':[{'urlButton':{'displayText':_0x36f452(0x198),'url':'https://www.youtube.com/c/DRIPSOFC'}},{'urlButton':{'displayText':_0x36f452(0x19b),'url':'https://github.com/zim-bot/zimbot-v3'}}]}}}),{'userJid':m[_0x36f452(0x19d)]});function _0x131e(_0xb57061,_0x1d708f){const _0x186b37=_0x186b();return _0x131e=function(_0x131e55,_0x4245fa){_0x131e55=_0x131e55-0x193;let _0x47b1da=_0x186b37[_0x131e55];return _0x47b1da;},_0x131e(_0xb57061,_0x1d708f);}ZimBotInc[_0x36f452(0x1a3)](m[_0x36f452(0x19d)],template['message'],{'messageId':template[_0x36f452(0x1a1)]['id']});
                 }
@@ -8155,7 +8155,7 @@ let hao = randomNomor(200)
 ┃https://tinyurl.com/2bghgjfx
 ┃
 ┃━━━━━━━━━━━━━━━━━━┃
-┃                  ᴢɪᴍʙᴏᴛɪɴᴄ
+┃                  ZxynnBotz
 ╰━━━━━━━━━━━━━━━━━━╯
 
      
